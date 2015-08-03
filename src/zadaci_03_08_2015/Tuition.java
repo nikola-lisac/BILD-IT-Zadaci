6bed46c343d9edfb3f8b7a3c3cef8206d217dc6b
Nikola Lisicic <nikola.lisicic@hotmail.com>
2015-08-03 20:06:40 +0200

    update

--- src/zadaci_03_08_2015/Tuition.java
+++ src/zadaci_03_08_2015/Tuition.java
@@ -12,18 +12,20 @@ public class Tuition {
 	public static void calculateTuition(double tuition, int years) {
 		double rate = 0.05;//kamata
 		
-		for (int i = 0; i < years; i++) {
+		for (int i = 1; i <= years; i++) {
 			tuition += tuition * rate;//racunanje skolarine 
+			System.out.printf("Skolarina za " + i + ". godinu ce iznositi: %.2f", tuition);//ispis rezultata
+			System.out.println();
 		}
 
 		//ispis rezultata
-		System.out.printf("Skolarina za " + years + " godina ce iznositi: %.2f", tuition);
 	}
 
 	public static void main(String[] args) {
 		
 		//izracunaj skolarinu nakon 10 godina ako je skolarina za godinu dana 10 000
 		calculateTuition(10000, 10);
+		
 	}
 
 }
