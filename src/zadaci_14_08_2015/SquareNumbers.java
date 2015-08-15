9130242aae50125e8b180d0c6a024a1690bd1f95
Nikola Lisicic <nikola.lisicic@hotmail.com>
2015-08-15 15:52:09 +0200

    Update

--- src/zadaci_14_08_2015/SquareNumbers.java
+++ src/zadaci_14_08_2015/SquareNumbers.java
@@ -1,36 +1,37 @@
 package zadaci_14_08_2015;
 
 import java.math.BigInteger;
+
 /**
- * 10.17
- * (Square numbers) Find the first ten square numbers that are greater than Long.MAX_VALUE.
- *  A square number is a number in the form of n2. For example, 4, 9, and 16 are square numbers. 
- *  Find an efficient approach to run your program fast.
+ * 10.17 (Square numbers) Find the first ten square numbers that are greater
+ * than Long.MAX_VALUE. A square number is a number in the form of n2. For
+ * example, 4, 9, and 16 are square numbers. Find an efficient approach to run
+ * your program fast.
+ * 
  * @author Nikola Lisicic
  *
  */
 public class SquareNumbers {
 
 	public static void main(String[] args) {
-		// square root of Long.MAX_VALUE in the string format without decimal point
-		String squareRootOfMaxLong = String.format("%.0f",Math.sqrt(Long.MAX_VALUE));
-		BigInteger number = new BigInteger(squareRootOfMaxLong);//BigInteger with the value of Long.MAX_VALUE
-		BigInteger maxLong = new BigInteger(Long.MAX_VALUE+"");
-		int count = 0;
+		// square root of Long.MAX_VALUE in the string format rounded to higher value
+		String squareRootOfMaxLong =(long) Math.ceil(Math.sqrt(Long.MAX_VALUE)) + "";
 		
-		System.out.println("Square numbers greater than "+Long.MAX_VALUE);
+		//numbber with the value of square root of Long.MaLong.MAX_VALUE
+		BigInteger number = new BigInteger(squareRootOfMaxLong);
+		int count = 0;
+
+		System.out.println("Square numbers greater than " + Long.MAX_VALUE);
 		while (count < 10) {
-			
-			//squared number
+
+			// squared number
 			BigInteger numberSquared = number.multiply(number);
-			
-			//if the squared number is greater than max long
-			if(numberSquared.compareTo(maxLong) >=0){
-				//print squared number
-				System.out.println(count + ". "+ numberSquared);
-				count++;
-			}
-			//increment the number
+
+			// print squared number
+			System.out.println(count + ". " + numberSquared);
+			count++;
+
+			// increment the number
 			number = number.add(BigInteger.ONE);
 		}
 	}
