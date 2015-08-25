6a7e249b9cd44e3d63bab10858a76aa6a9c12e14
Nikola Lisicic <nikola.lisicic@hotmail.com>
2015-08-25 14:25:58 +0200

    Update

--- src/zadaci_22_23_08_2015/Zadatak01Format.java
+++ src/zadaci_22_23_08_2015/Zadatak01Format.java
@@ -1,30 +1,46 @@
 package zadaci_22_23_08_2015;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.util.Scanner;
-
+/**
+ * (Reformat Java source code) Write a program that converts the Java source
+ * code from the next-line brace style to the end-of-line brace style.
+ * @author Nikola Lisicic
+ *
+ */
 public class Zadatak01Format {
 
 	public static void main(String[] args) {
-		if (args.length == 1) {
+		if (args.length == 0) {
+			System.out.println("Enter a file name as the first argument!");
+			System.exit(0);
+		}
+		else {
 			try {
+				//name of the source file
 				String fileName = args[0];
 				File file = new File(fileName);
 				Scanner input = new Scanner(file);
-				String string = "";
+				//variable which will contain all the the text from the file
+				String sourceFileText = "";
+				//go through the file and add all lines to the string
 				while (input.hasNext()) {
-					string += input.nextLine() + "\n";
+					sourceFileText += input.nextLine() + "\n";
 
 				}
-				string = string.replaceAll("\\)\\s*\\{", ") {");
-				string = string.replaceAll("\\s*\\{", " {");
+				//correct the formating
+				sourceFileText = sourceFileText.replaceAll("\\)\\s*\\{", ") {");
+				sourceFileText = sourceFileText.replaceAll("\\s*\\{", " {");
 
 				input.close();
-				PrintWriter pw = new PrintWriter(file);
-				pw.append(string);
+				//overwrite the file with formated text
+				PrintWriter pw = new PrintWriter(new FileOutputStream(file));
+				pw.append(sourceFileText);
 				pw.close();
 				System.out.println("Source code has been reformated.");
+				
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
